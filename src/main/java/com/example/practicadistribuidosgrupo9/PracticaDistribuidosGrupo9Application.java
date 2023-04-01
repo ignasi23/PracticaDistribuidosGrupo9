17fe345aa8dd5eb47b6a3d957ffad66e3a71c4e9
Ignacio Díaz Nieto <igdinieto@gmail.com>
2023-04-01 22:09:27 +0200

    Initial commit

---
+++ src/main/java/com/example/practicadistribuidosgrupo9/PracticaDistribuidosGrupo9Application.java
@@ -0,0 +1,13 @@
+package com.example.practicadistribuidosgrupo9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaDistribuidosGrupo9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaDistribuidosGrupo9Application.class, args);
+	}
+
+}
