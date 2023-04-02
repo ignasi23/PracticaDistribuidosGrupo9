78a149dd493c56f0c267e3aa5834586eb6b1ebdb
Ignacio Díaz Nieto <igdinieto@gmail.com>
2023-04-02 11:27:49 +0200

    Cambios

--- src/main/java/com/example/practicadistribuidosgrupo9/PracticaDistribuidosGrupo9Application.java
+++ src/main/java/com/example/practicadistribuidosgrupo9/PracticaDistribuidosGrupo9Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PracticaDistribuidosGrupo9Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PracticaDistribuidosGrupo9Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaDistribuidosGrupo9Application.class, args);
+    }
 
 }
