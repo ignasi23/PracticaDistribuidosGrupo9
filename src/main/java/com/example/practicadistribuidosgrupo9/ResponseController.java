48f4f2cf60a0e8c44ec886bb9340a932c84c692e
Ignacio Díaz Nieto <igdinieto@gmail.com>
2023-04-07 11:54:48 +0200

    Ahora cuando creas un formulario se envia con un mapa en memoria y te pone un mensaje de enviado correctamente

---
+++ src/main/java/com/example/practicadistribuidosgrupo9/ResponseController.java
@@ -0,0 +1,32 @@
+package com.example.practicadistribuidosgrupo9;
+
+// import com.example.model.Response;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/responses")
+public class ResponseController {
+
+    private List<Response> responses = new ArrayList<>();
+
+    @PostMapping("/submit")
+    public String submitResponse(Response response, RedirectAttributes redirectAttributes) {
+        responses.add(response);
+        redirectAttributes.addFlashAttribute("successMessage", "¡Mensaje enviado correctamente!");
+        return "redirect:/contact";
+    }
+
+    @GetMapping
+    public String viewResponses(Model model) {
+        model.addAttribute("responses", responses);
+        return "responses";
+    }
+}
