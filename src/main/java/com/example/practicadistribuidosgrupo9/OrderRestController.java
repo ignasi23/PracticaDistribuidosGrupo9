eac3e4c653f3207ae30aa9504279df8ca6356b8c
Ignacio Díaz Nieto <igdinieto@gmail.com>
2023-04-11 23:30:20 +0200

    OrderRestController y OrderService nuevos

--- src/main/java/com/example/practicadistribuidosgrupo9/OrderRestController.java
+++ src/main/java/com/example/practicadistribuidosgrupo9/OrderRestController.java
@@ -5,8 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api")
 public class OrderRestController {
@@ -18,39 +16,39 @@ public class OrderRestController {
     private UserService userService;
 
     @PostMapping("/orders")
-    public ResponseEntity<Order> createOrder(@CookieValue(name = "user", defaultValue = "") String userEmail, @RequestBody Order order) {
+    public ResponseEntity<String> createOrder(@CookieValue(name = "user", defaultValue = "") String userEmail, @RequestBody Order order) {
         Order createdOrder = orderService.createOrder(userEmail, order);
         if (createdOrder != null) {
-            return new ResponseEntity<>(createdOrder, HttpStatus.CREATED);
+            return new ResponseEntity<>("Order created successfully", HttpStatus.CREATED);
         }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>("Failed to create order", HttpStatus.BAD_REQUEST);
     }
 
     @PostMapping("/reportOrder")
     public ResponseEntity<String> reportOrder(@CookieValue(name = "user", defaultValue = "") String userEmail, @RequestBody OrderReport orderReport) {
         boolean success = orderService.reportOrder(userEmail, orderReport.getOrderID(), orderReport.getReportMsg());
         if (success) {
-            return new ResponseEntity<>("Ok", HttpStatus.OK);
+            return new ResponseEntity<>("Order reported successfully", HttpStatus.OK);
         }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>("Failed to report order", HttpStatus.BAD_REQUEST);
     }
 
     @GetMapping("/orders/{orderId}")
-    public ResponseEntity<Order> getOrder(@CookieValue(name = "user", defaultValue = "") String userEmail, @PathVariable String orderId) {
+    public ResponseEntity<String> getOrder(@CookieValue(name = "user", defaultValue = "") String userEmail, @PathVariable String orderId) {
         Order order = orderService.getOrder(userEmail, orderId);
         if (order != null) {
-            return new ResponseEntity<>(order, HttpStatus.OK);
+            return new ResponseEntity<>("Order found: " + order.toString(), HttpStatus.OK);
         }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>("Order not found", HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/orders/{orderId}")
     public ResponseEntity<String> deleteOrder(@CookieValue(name = "user", defaultValue = "") String userEmail, @PathVariable String orderId) {
         boolean success = orderService.deleteOrder(userEmail, orderId);
         if (success) {
-            return new ResponseEntity<>("Order deleted", HttpStatus.OK);
+            return new ResponseEntity<>("Order deleted successfully", HttpStatus.OK);
         }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>("Failed to delete order", HttpStatus.NOT_FOUND);
     }
-
 }
+
