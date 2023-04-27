0d8137d9b80949db6e9f9e7404a8119c9d5550dc
nereuuss <129610392+nereuuss@users.noreply.github.com>
2023-04-27 17:17:52 +0200

    modificacion main, pom y properties para h2

--- src/main/java/com/example/practicadistribuidosgrupo9/PracticaDistribuidosGrupo9Application.java
+++ src/main/java/com/example/practicadistribuidosgrupo9/PracticaDistribuidosGrupo9Application.java
@@ -1,11 +1,22 @@
 package com.example.practicadistribuidosgrupo9;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLException;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class PracticaDistribuidosGrupo9Application {
-    public static void main(String[] args) {
-        SpringApplication.run(PracticaDistribuidosGrupo9Application.class, args);}
+    public static void main(String[] args) throws SQLException {
+        Server server = Server.createTcpServer().start(); // Create instance of Server and run
+        SpringApplication.run(PracticaDistribuidosGrupo9Application.class, args);
+        Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "");
+        conn.close();
+        server.stop();
+    }
+
 }
