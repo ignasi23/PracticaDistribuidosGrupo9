744e30883b86959df60d4a670a78170d4ca64ca0
nereuuss <129610392+nereuuss@users.noreply.github.com>
2023-04-29 17:54:31 +0200

    añadir los repositorios a cada clase

---
+++ src/main/java/com/example/practicadistribuidosgrupo9/CartRepository.java
@@ -0,0 +1,6 @@
+package com.example.practicadistribuidosgrupo9;
+
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface CartRepository extends JpaSpecificationExecutor<CartService>/*JpaRepository<CartService, >*/ {
+}
